6b2f0a032e6d8200b2d26f5ea0fca27bd3ac550f
Toby Siu <siu.toby@gmail.com>
2025-07-28 23:50:05 +0800

    Final

--- logic.js
+++ logic.js
@@ -5,6 +5,8 @@ let playerNum = -1;
 let chosenNumber = -1;
 let choiceStatus = false;
 let guessStatus = false;
+let playerWins = 0;
+let botWins = 0;
 const choiceButtons = document.querySelectorAll(".playerchoice");
 const guessButtons = document.querySelectorAll(".playerguess");
 function pinput(pnum){
@@ -16,6 +18,7 @@ function binput(){
     const x = [0, 5, 10];
     const y = x[Math.floor(Math.random()*3)]
     console.log("bot input: " + y);
+    changeText("botChoice", "Bot choice: " + y);
     return y;
 
 }
@@ -24,56 +27,115 @@ function chosenNum(cnum){
     chosenNumber = cnum;
     console.log("Chosen Predicted Number: " + cnum);
 }
-function turn(){
-    turnvar *= -1;
-    console.log("turn()");
-}
-function changeText(element, text){
-    document.getElementById(element).innerHTML = text;
-    console.log("changed text");
-}
 /*
 1 is player, -1 is bot.
 False = lose, True = win.
  */
+function turn(){
+    turnvar *= -1;
+    if (turnvar > 0){
+        changeText("turn", "Turn: Player");
+    }
+    else {
+        changeText("turn", "Turn: Bot");
+    }
+    console.log("turn()");
+}
+function changeText(id, text){
+    document.getElementById(id).innerHTML = text;
+    console.log("changed text");
+}
+function changeImg(id, pic){
+    document.getElementById(id).src = pic;
+}
+function hideImg(id, hide){
+    if (hide){
+       document.getElementById(id).style.display = "none";
+        console.log("Pictures hidden");
+    }
+    else {
+        document.getElementById(id).style.display = "";
+        console.log("Pictures shown");
+    }
+}
+function disableChoiceButton(disable){
+    if (disable){
+        choiceButtons.forEach(playerchoice => {
+            playerchoice.disabled = true;
+        });
+    }
+    else {
+        choiceButtons.forEach(playerchoice => {
+            playerchoice.disabled = false;
+        });
+    }
+}
+function disableGuessButton(disable){
+    if (disable){
+        guessButtons.forEach(playerguess => {
+            playerguess.disabled = true;
+        });
+    }
+    else {
+        guessButtons.forEach(playerguess => {
+            playerguess.disabled = false;
+        });
+    }
+}
+
+
 function determineWinner(binput, pinput, chosenNum, turn){
     console.log("Running determineWinner().");
     if (turn == 1){
         if (binput + pinput == chosenNum) {
             win = true;
+            changeImg('TW', 'TWwin.jpg');
+            playerWins++;
         }
         else {
             win = false;
+            changeImg('TW', 'TWlose.jpg');
+            botWins++;
         }
     }
     else{
         if (binput + pinput == chosenNum) {
             win = false;
+            changeImg('TW', 'TWlose.jpg');
+            botWins++;
         }
         else {
             win = true;
+            changeImg('TW', 'TWwin.jpg');
+            playerWins++;
         }
     }
     console.log("Player wins: " + win);
 }
 const choicefunc = () => {
-        choiceStatus = true
-        console.log("choice button clicked. Choice Clicked Status: " + choiceStatus);
-    };
-    const guessfunc = () => {
-        guessStatus = true;
-        console.log("Guess button clicked, Guess Clicked Status: " + guessStatus);
-    }
+    disableChoiceButton(true);
+    choiceStatus = true
+    changeText("playersChoice", "Player choice: " + playerNum);
+};
+const guessfunc = () => {
+    disableGuessButton(true);
+    guessStatus = true;
+    changeText("playersguess", "Player guess: " + chosenNumber);
+}
 function round(){
     choiceButtons.forEach(playerchoice => playerchoice.addEventListener("click", choicefunc));
     guessButtons.forEach(playerguess => playerguess.addEventListener("click", guessfunc));
+    hideImg("TW", true);
     console.log("----------------------------");
     win = null;
     console.log("Current turn: " + turnvar);
-    buttonsPressed();
-    //block other buttons
-    console.log("round finished.");
-    console.log("Player win: " + win);
+    checkButtons();
+}
+function clearWords(){
+    changeText("winning status", "");
+    changeText("playersChoice", "Player choice: ");
+    changeText("botChoice", "Bot choice: ");
+    changeText("playersguess", "Player guess: ");
 }
 function reset(){
     console.log("resetting...");
@@ -82,82 +144,48 @@ function reset(){
     //unblock buttons and remove event listeners
     choiceButtons.forEach(playerchoice => {playerchoice.removeEventListener("click", choicefunc)});
     guessButtons.forEach(playerguess => {playerguess.removeEventListener("click", guessfunc)});
+    clearWords();
+    hideImg("TW", true);
+    disableChoiceButton(false);
+    disableGuessButton(false);
     console.log("finished resetting.");
 }
-/*
-function buttonPromise(){
-    console.log("---------------------------");
-    console.log("Buttonpromise();");
-    return new Promise((resolve) =>{
-        console.log("Promise created");
-        const interval = setInterval(() => {
-            console.log("interval running...");
-            if (guessStatus && choiceStatus){
-                console.log("Interval cleared");
-                clearInterval(interval);
-                resolve("Promise resolved");
-                console.log("Promise resolved.");
-            }}, 100);
-    });
-}
-*/
+
 function checkButtons(){
-    const intervall = setInterval(() => {
+    const interval = setInterval(() => {
         if (guessStatus && choiceStatus){
-            clearInterval(intervall);
-            buttonsPressedd();
+            clearInterval(interval);
+            console.log("both buttons pressed");
+            buttonsPressed();
+
         }
     }, 200);
 }
-function buttonsPressedd(){
-    determineWinner(binput(), playerNum, chosenNumber, turnvar);
+function delay(){
     turn();
     reset();
-}
-/*
-async function buttonsPressed(){
-    console.log("-----------------------------");
-    console.log("buttonspressed();");
-    try {
-        console.log("try");
-        const response = await buttonPromise();
-        console.log("Both buttons pressed.");
-        determineWinner(binput(), playerNum, chosenNumber, turnvar);
-        turn();
-        reset();
-    }
-    catch{
-        console.log("Buttons not pressed." + choiceStatus + guessStatus);
-    }
-}
-*/
-
-
-function game(){
-    while(true){
-        console.log("round 1")
-        round();
-        if(win){
-            console.log("round 2");
-            changeText("winning status", "Win one more!");
-            round();
-            if(win){
-                changeText("winning status", "You win! :)");
-                turn();
-                break;
-            }
-        }
-        else{
-            console.log("round 2");
-            changeText("winning status", "Last Chance..");
-            round();
-            if(!win){
-                changeText("winning status", "You Lose.. :(");
-                turn();
-                break;
-            }
-        }
-    }
+    console.log("round finished.");
     games++;
+    round();
+}
+function buttonsPressed(){
+    determineWinner(binput(), playerNum, chosenNumber, turnvar);
+    hideImg("TW", false);
+    checkRounds();
+    setTimeout(delay, 2000);
+}
+//TODO: Let player choose how many rounds to play to win the game 
+function checkRounds(){
+    if(win){
+        changeText("winning status", "You Win!")
+    }
+    else if(!win){
+        changeText('winning status', "You lose..");
+    }
+    else {
+        changeText("winning status", "Winning status: ERROR");
+        console.log("ERROR: " + win + botWins + playerWins);
+    }
+    changeText("rounds", "Total rounds: " + games);
 }
 round();
